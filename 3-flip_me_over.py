2a0038c679675cb0cb3420806680cb6679efd438
Thergermanblob <thegermanblob@gmail.com>
2021-11-30 15:04:48 -0400

    Add task 3

---
+++ 3-flip_me_over.py
@@ -0,0 +1,12 @@
+def matrix_transpose(matrix):
+    """ Function to transpose a matrix """
+    rows = len(matrix)
+    colums = len(matrix[0])
+    print(f"rows {rows} colum {colums}")
+    r = []
+    result = [[0 for x in range(rows)] for y in range(colums)]
+   
+    for i in range(0, len(matrix[0])):
+        for j in range(0, len(matrix)):
+            result[i][j] = matrix[j][i]
+    return result
