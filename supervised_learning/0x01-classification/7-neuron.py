02b69b00b3536f4a88d3df836c442b856c4def50
Thergermanblob <thegermanblob@gmail.com>
2022-01-03 18:36:26 -0400

    Add Task 7

---
+++ supervised_learning/0x01-classification/7-neuron.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+""" Module containing a Class of a neuron """
+import numpy as np
+import matplotlib as plt
+
+class Neuron():
+    """ Class representing a neuron """
+    def __init__(self, nx) -> None:
+        """ Initialization method """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(size=(1,nx))
+        self.__b = 0
+        self.__A = 0
+
+    def forward_prop(self, X):
+        """ Calculates the foward propagation """
+        if type(X) is not np.ndarray:
+            raise TypeError("X not array")
+        z = -1 * (np.dot(self.__W, X) + self.__b)
+        self.__A = 1/(1 + np.exp(z))
+        return self.__A
+
+    @property
+    def W(self):
+        """ Getter method for weights """
+        return self.__W
+
+    @property
+    def b(self):
+        """ Getter method for bias """
+        return self.__b
+
+    @property
+    def A(self):
+        """ Getter method for activated output """
+        return self.__A
+
+    def cost(self, Y, A):
+        """ calculates cost of the model using logistic regression  """
+        return -(Y * np.log(np.where(A== 0, 0.00000001, A)) + (1 - Y)* np.log(1.0000001 - A)).mean()
+
+
+    def evaluate(self, X, Y):
+        """ Evaluates the neurons prediction """
+        prediction = self.forward_prop(X) 
+        cost = self.cost(Y, prediction)
+        return np.where(prediction >= 0.5, 1, 0), cost
+
+    def gradient_descent(self, X,Y,A,alpha=0.05):
+        """ Calculates one pass of gradient descent on the neuron """ #?
+        m = len(Y[0])
+        grad = np.matmul(X, (A - Y).T) / m 
+        self.__W -= alpha * grad.T
+        self.__b -= alpha * np.average(A - Y) # Whats right gets a lower bias on
+
+
+    def train(self, X, Y, iterations=5000, alpha=0.05, verbose=True,
+              graph=True, step=100):
+        """ Trains neuron  may show graph if verbose true"""
+        if type(iterations) is not int:
+            raise TypeError("iterations must be an integer")
+        if iterations <= 0:
+            raise ValueError("iterations must be a positive integer")
+        if type(alpha) is not float:
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+        if verbose or graph:
+            if type(step) is not int:
+                raise TypeError("step must be an integer")
+        if step < 1:
+            raise ValueError("step must be a positive integer")
+        itrcount = 0
+        losses = []
+        graphx = []
+        while itrcount < iterations:
+            A = self.forward_prop(X)
+            if verbose and not (itrcount % step):
+                print("Cost after {} iterations: {}"
+                      .format(itrcount, self.cost(Y, A)))
+                if graph:
+                    losses.append(self.cost(Y, A))
+                    graphx.append(itrcount)
+            self.gradient_descent(X, Y, A, alpha)
+            itrcount += 1
+        self.forward_prop(X)
+        if verbose:
+            print("Cost after {} iterations: {}"
+                  .format(itrcount, self.cost(Y, self.__A)))
+        if graph:
+            plt.plot(graphx, losses, "b-")
+            plt.xlabel("iteration")
+            plt.ylabel("cost")
+            plt.title("Training Cost")
+        return self.evaluate(X, Y)
