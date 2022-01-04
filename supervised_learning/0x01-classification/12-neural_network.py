3f68af8206a80f4a752d8ce88645c86262028dec
Thergermanblob <thegermanblob@gmail.com>
2022-01-03 20:08:53 -0400

    Big push

---
+++ supervised_learning/0x01-classification/12-neural_network.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""Simple neural network class"""
+
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """ Represnets simple neural network """
+
+    def __init__(self, nx, nodes):
+        """ Initialization method """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.W1 = np.ndarray((nodes, nx))
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.W2 = np.ndarray((1, nodes))
+        self.W2[0] = np.random.normal(size=nodes)
+        self.b1 = np.zeros((nodes, 1))
+        self.b2 = 0
+        self.A1 = 0
+        self.A2 = 0
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def b2(self):
+        return self.__b2
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def A2(self):
+        return self.__A2
+
+    def forward_prop(self, X):
+        """ Calculate forward propagation for neural network """
+        self.__A1 = np.dot(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-1 * self.__A1))
+        self.__A2 = (np.dot(self.__W2, self.__A1) +
+                     self.__b2)
+        self.__A2 = 1 / (1 + np.exp(-1 * self.__A2))
+        return self.__A1, self.__A2
+    
+    def cost(self, Y, A):
+        """ Calculate cost of the neural network """
+        return -(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)).mean()
+
+    def evaluate(self, X, Y):
+        """ Evaluate the neural network """
+        return (self.forward_prop(X)[1].round().astype(int),
+                self.cost(Y, self._A2))
--- supervised_learning/0x01-classification/6-neuron.py
+++ supervised_learning/0x01-classification/6-neuron.py
@@ -2,15 +2,17 @@
 """ Module containing a Class of a neuron """
 import numpy as np
 
+
 class Neuron():
     """ Class representing a neuron """
+
     def __init__(self, nx) -> None:
         """ Initialization method """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.__W = np.random.normal(size=(1,nx))
+        self.__W = np.random.normal(size=(1, nx))
         self.__b = 0
         self.__A = 0
 
@@ -39,18 +41,28 @@ class Neuron():
 
     def cost(self, Y, A):
         """ calculates cost of the model using logistic regression  """
-        return -(Y * np.log(np.where(A== 0, 0.00000001, A)) + (1 - Y)* np.log(1.0000001 - A)).mean()
-
+        return -(Y * np.log(np.where(A == 0, 0.00000001, A)) + (1 - Y) * np.log(1.0000001 - A)).mean()
 
     def evaluate(self, X, Y):
         """ Evaluates the neurons prediction """
-        prediction = self.forward_prop(X) 
+        prediction = self.forward_prop(X)
         cost = self.cost(Y, prediction)
         return np.where(prediction >= 0.5, 1, 0), cost
 
-    def gradient_descent(self, X,Y,A,alpha=0.05):
-        """ Calculates one pass of gradient descent on the neuron """ #?
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """ Calculates one pass of gradient descent on the neuron 
+            Args:
+                X (np.ndarray): input data
+                Y (np.ndarray): correct labels for the input data
+                A (np.ndarray): activated output of the neuron with
+                shape (1, m) where m is the number of examples
+                alpha (float): learning rate.
+        """  # ?
         m = len(Y[0])
-        grad = np.matmul(X, (A - Y).T) / m 
+        grad = np.matmul(X, (A - Y).T) / m
         self.__W -= alpha * grad.T
-        self.__b -= alpha * np.average(A - Y) # Whats right gets a lower bias on
+        # Whats right gets a lower bias on
+        self.__b -= alpha * np.average(A - Y)
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """ Trains the neuron """
