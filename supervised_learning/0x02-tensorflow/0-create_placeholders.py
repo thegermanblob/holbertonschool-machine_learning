ea2dfbc277876b4b9498222fb8932efd36a189d5
Thegermanblob <thegermanblob@gmail.com>
2022-01-07 22:10:07 -0400

    Fix task 0

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -3,8 +3,8 @@
 import tensorflow.compat.v1 as tf
 tf.disable_eager_execution()
 
-def create_placeholders(nx, clases):
+def create_placeholders(nx, classes):
     """ creates and returns place holders for data """
-    x = tf.placeholder("float", shape=(None,nx), name="x")
-    y = tf.placeholder("float", shape=(None,clases), name="y")
+    x = tf.placeholder("float", shape=[None,nx], name="x")
+    y = tf.placeholder("float", shape=[None,classes], name="y")
     return x, y
