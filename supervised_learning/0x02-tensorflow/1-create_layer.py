dd01cfc64635a589b855a02822690c5c9a98ae8e
Thergermanblob <thegermanblob@gmail.com>
2022-01-07 23:08:30 -0400

    Format pycode

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -2,9 +2,10 @@
 import tensorflow.compat.v1 as tf
 tf.disable_eager_execution()
 
+
 def create_layer(prev, n, activation):
     """ Creates a layer for the network """
     init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    layer = tf.layers.Dense(units=n, activation=activation, kernel_initializer=init, name="layer")
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=init, name="layer")
     return layer(prev)
-
