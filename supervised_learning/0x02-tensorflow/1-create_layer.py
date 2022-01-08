81879758184e1b54c5edf617f2883fba15a4d436
Thergermanblob <thegermanblob@gmail.com>
2022-01-07 22:56:45 -0400

    Add initialization of weights

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -4,6 +4,7 @@ tf.disable_eager_execution()
 
 def create_layer(prev, n, activation):
     """ Creates a layer for the network """
-    layer = tf.layers.Dense(units=n, activation=activation)
+    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    layer = tf.layers.Dense(units=n, activation=activation, kernel_initializer=init)
     return layer(prev)
 
