1d65307b87bcdbc77427df1f34a7e7f73600ce97
Thergermanblob <thegermanblob@gmail.com>
2022-01-07 23:02:47 -0400

    Fix name of layer

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -5,6 +5,6 @@ tf.disable_eager_execution()
 def create_layer(prev, n, activation):
     """ Creates a layer for the network """
     init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    layer = tf.layers.Dense(units=n, activation=activation, kernel_initializer=init)
+    layer = tf.layers.Dense(units=n, activation=activation, kernel_initializer=init, name="layer")
     return layer(prev)
 
