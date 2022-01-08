211109ed62c0d87f3ecb86b7d72a9a4e10284758
Thergermanblob <thegermanblob@gmail.com>
2022-01-07 22:54:31 -0400

    fix 1

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -4,7 +4,6 @@ tf.disable_eager_execution()
 
 def create_layer(prev, n, activation):
     """ Creates a layer for the network """
-    tf.keras.initializers.VarianceScaling(mode='fan_avg')
     layer = tf.layers.Dense(units=n, activation=activation)
     return layer(prev)
 
