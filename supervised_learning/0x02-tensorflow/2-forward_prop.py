1931640433b4e94e21859977d81339ac44cc2a50
Thergermanblob <thegermanblob@gmail.com>
2022-01-09 04:42:53 -0400

    Fix task 2

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -3,6 +3,7 @@ import tensorflow.compat.v1 as tf
 create_layer = __import__('1-create_layer').create_layer
 tf.disable_eager_execution()
 
+
 def forward_prop(x, layer_sizes=[], activations=[]):
     """
     creates the forward propagation graph for the neural network 
